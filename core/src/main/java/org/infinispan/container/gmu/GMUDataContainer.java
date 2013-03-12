0c8aadf69566d95752aff55d02d12fdf60be095b
nmldiegues <nmld@ist.utl.pt>
2013-03-12 17:13:31 +0000

    Parametrize visible reads.

--- core/src/main/java/org/infinispan/container/gmu/GMUDataContainer.java
+++ core/src/main/java/org/infinispan/container/gmu/GMUDataContainer.java
@@ -165,7 +165,12 @@ public class GMUDataContainer extends AbstractDataContainer<GMUDataContainer.Dat
          while (lockManager.isLocked(k)) { }
       }
       
-      VersionEntry<InternalCacheEntry> entry = chain.get(getReadVersion(version, false));
+      VersionEntry<InternalCacheEntry> entry;
+      if (visibleRead) {
+         entry = chain.get(getReadVersion(version, false));
+      } else {
+         entry = chain.get(getReadVersion(version, true));
+      }
 
       if (log.isTraceEnabled()) {
          log.tracef("DataContainer.peek(%s,%s) => %s", k, version, entry);
