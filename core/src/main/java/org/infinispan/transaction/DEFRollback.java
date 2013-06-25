320206efbe6fb79382af736e2d7e5b867ff69b87
nmldiegues <nmld@ist.utl.pt>
2013-06-25 15:37:45 +0100

    Missed implementation of DEFRollback

--- core/src/main/java/org/infinispan/transaction/DEFRollback.java
+++ core/src/main/java/org/infinispan/transaction/DEFRollback.java
@@ -5,24 +5,35 @@ import java.util.Set;
 
 import org.infinispan.Cache;
 import org.infinispan.distexec.DistributedCallable;
+import org.infinispan.transaction.tm.DummyTransactionManager;
 import org.infinispan.transaction.xa.GlobalTransaction;
 
 public class DEFRollback implements DistributedCallable, Serializable {
 
-    private GlobalTransaction tx;
-    
-    public DEFRollback(GlobalTransaction tx) {
-	this.tx = tx;
-    }
+   private GlobalTransaction tx;
+   private Cache cache;
 
-    @Override
-    public Object call() throws Exception {
-	// TODO resume, prepare, suspend, return result
-	return null;
-    }
+   public DEFRollback(GlobalTransaction tx) {
+      this.tx = tx;
+   }
 
-    @Override
-    public void setEnvironment(Cache cache, Set inputKeys) {
-    }
+   @Override
+   public Object call() throws Exception {
+      LocalTransaction local = this.cache.getAdvancedCache().getTxTable().getLocalTransaction(tx);
+      DummyTransactionManager tm = (DummyTransactionManager) this.cache.getAdvancedCache().getTransactionManager();
+      javax.transaction.Transaction jpaTx = local.getTransaction();
+      tm.resume(jpaTx);
+      try {
+         tm.rollback();
+      } finally {
+         tm.suspend();
+      }
+      return null;
+   }
+
+   @Override
+   public void setEnvironment(Cache cache, Set inputKeys) {
+      this.cache = cache;
+   }
 
 }
